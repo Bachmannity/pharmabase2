f44ef545a7c3b9f1e3d4a285e8f7f33ade86d24d
Bachmannity <39768857+Bachmannity@users.noreply.github.com>
2023-12-05 10:14:42 +0000

    Added dynamic data

--- app.py
+++ app.py
@@ -2,10 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+DATABASE = [
+  {
+    'id': 890,
+    'therapy': 'Uplinza',
+    'disease': 'NMOSD',
+    'benefit': 'Unquantifiable'
+  },
+  {
+    'id': 670,
+    'therapy': 'Dupixent',
+    'disease': 'Asthma',
+    'benefit': 'None'
+  },
+  {
+    'id': 750,
+    'therapy': 'Rinvoq',
+    'disease': 'AD',
+    'benefit': 'Minor'
+  },
+  {
+    'id': 520,
+    'therapy': 'Vyvgart',
+    'disease': 'MG',
+    'benefit': 'Considerable'
+  }
+]
 
+@app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', database=DATABASE)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
