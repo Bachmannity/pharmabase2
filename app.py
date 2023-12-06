7e5032680003ddd0628cd9d258d3036120963dbb
Bachmannity <39768857+Bachmannity@users.noreply.github.com>
2023-12-06 14:20:44 +0000

    added dynamic pages for each assessment

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 from flask import Flask, render_template, jsonify
-from database import load_table_from_db
+from database import load_gba_dict_list_from_db, load_assessment_dict_from_db
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_pharmabase():
-  table = load_table_from_db("select * from gba")
-  return render_template('home.html',
-                         table=table,
+  gba_dict_list = load_gba_dict_list_from_db("SELECT * FROM gba")
+  return render_template("home.html",
+                         table=gba_dict_list,
                          company="Pharmabase")
 
-@app.route('/api/table')
+@app.route("/api/table")
 def list_table():
-  table = load_table_from_db("select * from gba")
-  return jsonify(table)
+  gba_dict_list = load_gba_dict_list_from_db("SELECT * FROM gba")
+  return jsonify(gba_dict_list)
 
-if __name__ == '__main__':
+@app.route("/assessment/<id>")
+def show_assessment(id):
+  assessment_dict = load_assessment_dict_from_db(id)
+  if not assessment_dict:
+    return "Not Found", 404
+  return render_template("assessmentpage.html",
+                         assessment=assessment_dict)
+
+if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -3,20 +3,27 @@ from sqlalchemy import create_engine, text
 import pandas as pd
 import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = "mysql+pymysql://9cweyz5agb1iua6z88uv:pscale_pw_KQYpIdeVoxwDnXyPvYQTL1usXK3P3SMVYB2p7d7sELZ@aws.connect.psdb.cloud/htainsights?charset=utf8mb4"
 
-engine = create_engine(
-  db_connection_string, 
-  connect_args={
-    "ssl": {
-      "ssl_ca": "/etc/ssl/cert.pem",
-    }
-  }
-)
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem",
+                       }})
 
-def load_table_from_db(query):
+
+def load_gba_dict_list_from_db(query):
   with engine.connect() as conn:
     df = pd.read_sql(query, conn)
-    table = df.to_dict('records')
-  print(table)
-  return table
+    dict_list = df.to_dict('records')
+  return dict_list
+
+
+def load_assessment_dict_from_db(id):
+  with engine.connect() as conn:
+    query = "SELECT * FROM gba WHERE id = " + str(id)
+    df = pd.read_sql(query, conn)
+    dict_list = df.to_dict('records')
+    if len(dict_list) == 0:
+      return None
+    else:
+      return dict_list[0]
