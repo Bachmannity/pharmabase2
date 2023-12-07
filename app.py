f97748c8c2adb3cb979a574f7d2c2d20ffb974fa
Bachmannity <39768857+Bachmannity@users.noreply.github.com>
2023-12-07 14:00:01 +0000

    build filtering elements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_gba_dict_list_from_db, load_assessment_dict_from_db
 
 app = Flask(__name__)
@@ -7,22 +7,29 @@ app = Flask(__name__)
 def hello_pharmabase():
   return render_template("home.html",
                           company="Pharmabase")
-  
-@app.route("/htainsights")
-def hta_insights():
-  gba_dict_list = load_gba_dict_list_from_db("SELECT * FROM gba")
-  return render_template("htainsights.html",
-                         table=gba_dict_list,
-                         company="Pharmabase")
 
-@app.route("/api/table")
+@app.route("/htainsights")
+def search_hta_insights():
+  return render_template("htainsightssearch.html")
+
+@app.route("/htainsights/results", methods=['post'])
+def hta_insights():
+  search_results = request.form
+  return render_template("htainsightsresults.html",
+                         search_results=search_results)
+
+#@app.route("/htainsights/results", )
+#def hta_insights():
+#  gba_dict_list = load_gba_dict_list_from_db("SELECT * FROM gba")
+#  return render_template("htainsightsresults.html",
+#                         gba_dict_list=gba_dict_list)
+
+@app.route("/htainsights/results/api/table")
 def list_table():
   gba_dict_list = load_gba_dict_list_from_db("SELECT * FROM gba")
   return jsonify(gba_dict_list)
 
-
-
-@app.route("/assessment/<id>")
+@app.route("/htainsights/results/assessment/<id>")
 def show_assessment(id):
   assessment_dict = load_assessment_dict_from_db(id)
   if not assessment_dict:
